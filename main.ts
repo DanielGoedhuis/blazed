8159c5b1ea7cd5acf76a31b8681f8e1b729e450d
DanielGoedhuis <77095626+DanielGoedhuis@users.noreply.github.com>
2021-12-15 09:48:45 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, mySprite)
+    mySprite.setImage(assets.image`myImage3`)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`myAnim2`,
+    200,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`myAnim0`,
+    200,
+    true
+    )
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`myImage3`, SpriteKind.Player)
+controller.moveSprite(mySprite, 50, 50)
+tiles.setTilemap(tilemap`level3`)
+scene.cameraFollowSprite(mySprite)
+forever(function () {
+	
+})
